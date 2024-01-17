c478b818c8b81b68847abefd8da467963277c4fe
hasanarpat <black_the_dragon@hotmail.com>
2024-01-17 15:38:38 +0300

    create desktop orientation for poster component

--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -4,21 +4,38 @@ import Link from 'next/link';
 
 const Poster = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.desc}>
-        <div className={styles.model}>
-          <p>Nike Style By</p>
-          <p>NIKE V2K RUN</p>
+    <>
+      <div className={styles.desktopContainer}>
+        <div className={styles.desc}>
+          <div className={styles.model}>
+            <p className={styles.title}>Nike Style By</p>
+            <h1 className={styles.subTitle}>NIKE V2K RUN</h1>
+          </div>
+          <p>Notice the retro inspired sneaker</p>
+          <Link
+            href="/"
+            className={styles.link}
+          >
+            Inspect
+          </Link>
         </div>
-        <p>Notice the retro inspired sneaker</p>
-        <Link
-          href="/"
-          className={styles.link}
-        >
-          Inspect
-        </Link>
       </div>
-    </div>
+      <div className={styles.mobileContainer}>
+        <div className={styles.desc}>
+          <div className={styles.model}>
+            <p>Nike Style By</p>
+            <p>NIKE V2K RUN</p>
+          </div>
+          <p>Notice the retro inspired sneaker</p>
+          <Link
+            href="/"
+            className={styles.link}
+          >
+            Inspect
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
